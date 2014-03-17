fac45b238a08e27eba1ae55e3277aa1ad42261ac
Cesar Augusto Garcia <cesar.garcia@mulesoft.com>
2014-03-17 14:13:49 -0300

    Refactoring integration test

--- src/test/java/org/mule/templates/integration/BusinessLogicIT.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicIT.java
@@ -1,7 +1,12 @@
 package org.mule.templates.integration;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static org.mule.templates.builders.SfdcObjectBuilder.anAccount;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -13,6 +18,9 @@ import org.mule.api.MuleEvent;
 import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
 import org.mule.streaming.ConsumerIterator;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.templates.builders.SfdcObjectBuilder;
+
+import com.sforce.soap.partner.SaveResult;
 
 /**
  * The objective of this class is to validate the correct behavior of the flows
@@ -20,18 +28,101 @@ import org.mule.tck.junit4.rule.DynamicPort;
  * 
  */
 public class BusinessLogicIT extends AbstractTemplateTestCase {
-	private static final String ACCOUNTS_COMPANY_A = "accountsFromOrgA";
-	private static final String ACCOUNTS_COMPANY_B = "accountsFromOrgB";
+	protected static final String TEMPLATE_NAME = "account-aggregation";
+
+	private static final String ACCOUNTS_FROM_ORG_A = "accountsFromOrgA";
+	private static final String ACCOUNTS_FROM_ORG_B = "accountsFromOrgB";
+
+	private List<Map<String, Object>> createdAccountsInA = new ArrayList<Map<String, Object>>();
+	private List<Map<String, Object>> createdAccountsInB = new ArrayList<Map<String, Object>>();
 
 	@Rule
-	public DynamicPort port = new DynamicPort ("http.port");
-	
+	public DynamicPort port = new DynamicPort("http.port");
+
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
+
+		createAccounts();
+	}
+
+	@SuppressWarnings("unchecked")
+	private void createAccounts() throws Exception {
+		SubflowInterceptingChainLifecycleWrapper flow = getSubFlow("createAccountInAFlow");
+		flow.initialise();
+
+		Map<String, Object> account = createAccount("A", 0);
+		createdAccountsInA.add(account);
+
+		MuleEvent event = flow.process(getTestEvent(createdAccountsInA,
+				MessageExchangePattern.REQUEST_RESPONSE));
+		List<SaveResult> results = (List<SaveResult>) event.getMessage()
+				.getPayload();
+		for (int i = 0; i < results.size(); i++) {
+			createdAccountsInA.get(i).put("Id", results.get(i).getId());
+		}
+
+		flow = getSubFlow("createAccountInBFlow");
+		flow.initialise();
+
+		account = createAccount("B", 0);
+		createdAccountsInB.add(account);
+
+		event = flow.process(getTestEvent(createdAccountsInB,
+				MessageExchangePattern.REQUEST_RESPONSE));
+		results = (List<SaveResult>) event.getMessage().getPayload();
+
+		for (int i = 0; i < results.size(); i++) {
+			createdAccountsInB.get(i).put("Id", results.get(i).getId());
+		}
+	}
+
+	protected Map<String, Object> createAccount(String orgId, int sequence) {
+		return SfdcObjectBuilder
+				.anAccount()
+				.with("Name",
+						buildUniqueName(TEMPLATE_NAME, "_Name_" + sequence
+								+ "_"))
+				.with("Industry", "Education")
+				.with("Description", "Some account description")
+				.build();
+	}
+
+	protected String buildUniqueName(String templateName, String name) {
+		String timeStamp = new Long(new Date().getTime()).toString();
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(name);
+		builder.append(templateName);
+		builder.append(timeStamp);
+
+		return builder.toString();
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDown() throws Exception {
+
+		deleteTestAccountFromSandBox(createdAccountsInA,
+				"deleteAccountFromAFlow");
+		deleteTestAccountFromSandBox(createdAccountsInB,
+				"deleteAccountFromBFlow");
+
+	}
+
+	protected void deleteTestAccountFromSandBox(
+			List<Map<String, Object>> createdAccounts, String deleteFlow)
+			throws Exception {
+		List<String> idList = new ArrayList<String>();
+
+		// Delete the created accounts in A
+		SubflowInterceptingChainLifecycleWrapper flow = getSubFlow(deleteFlow);
+		flow.initialise();
+		for (Map<String, Object> c : createdAccounts) {
+			idList.add((String) c.get("Id"));
+		}
+		flow.process(getTestEvent(idList,
+				MessageExchangePattern.REQUEST_RESPONSE));
+		idList.clear();
+
 	}
 
 	@Test
@@ -39,17 +130,26 @@ public class BusinessLogicIT extends AbstractTemplateTestCase {
 		SubflowInterceptingChainLifecycleWrapper flow = getSubFlow("gatherDataFlow");
 		flow.initialise();
 
-		MuleEvent event = flow.process(getTestEvent("", MessageExchangePattern.REQUEST_RESPONSE));
+		MuleEvent event = flow.process(getTestEvent("",
+				MessageExchangePattern.REQUEST_RESPONSE));
 		Set<String> flowVariables = event.getFlowVariableNames();
 
-		Assert.assertTrue("The variable accountsFromOrgA is missing.", flowVariables.contains(ACCOUNTS_COMPANY_A));
-		Assert.assertTrue("The variable accountsFromOrgB is missing.", flowVariables.contains(ACCOUNTS_COMPANY_B));
+		Assert.assertTrue("The variable accountsFromOrgA is missing.",
+				flowVariables.contains(ACCOUNTS_FROM_ORG_A));
+		Assert.assertTrue("The variable accountsFromOrgB is missing.",
+				flowVariables.contains(ACCOUNTS_FROM_ORG_B));
 
-		ConsumerIterator<Map<String, String>> accountsFromOrgA = event.getFlowVariable(ACCOUNTS_COMPANY_A);
-		ConsumerIterator<Map<String, String>> accountsFromOrgB = event.getFlowVariable(ACCOUNTS_COMPANY_B);
+		ConsumerIterator<Map<String, String>> accountsFromOrgA = event
+				.getFlowVariable(ACCOUNTS_FROM_ORG_A);
+		ConsumerIterator<Map<String, String>> accountsFromOrgB = event
+				.getFlowVariable(ACCOUNTS_FROM_ORG_B);
 
-		Assert.assertTrue("There should be users in the variable usersFromOrgA.", accountsFromOrgA.size() != 0);
-		Assert.assertTrue("There should be users in the variable usersFromOrgB.", accountsFromOrgB.size() != 0);
+		Assert.assertTrue(
+				"There should be accounts in the variable accountsFromOrgA.",
+				accountsFromOrgA.size() != 0);
+		Assert.assertTrue(
+				"There should be accounts in the variable accountsFromOrgB.",
+				accountsFromOrgB.size() != 0);
 
 	}
 
--- src/test/java/org/mule/templates/transformers/SFDCAccountsMergeTest.java
+++ src/test/java/org/mule/templates/transformers/SFDCAccountsMergeTest.java
@@ -15,7 +15,7 @@ import org.mule.DefaultMuleMessage;
 import org.mule.api.MuleContext;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
-import org.mule.templates.builders.SfdcEntityBuilder;
+import org.mule.templates.builders.SfdcObjectBuilder;
 import org.mule.templates.transformers.SFDCAccountsMerge;
 
 @RunWith(MockitoJUnitRunner.class)
